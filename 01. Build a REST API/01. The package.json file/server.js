982bda0ed310549f096b85d4dad234579a9ea039
Riya Solanki <riyahsolanki@gmail.com>
2026-04-06 22:23:49 -0500

    intro

---
+++ 01. Build a REST API/01. The package.json file/server.js
@@ -0,0 +1,7 @@
+console.log('We now have a package.json!')
+
+/*
+Challenge:
+1. Create a package.json file by typing "npm init" in the terminal.
+2. Answer the questions using the info in project_details.md.
+*/
